ecc5ce23ea481108ec7f7e00890ee2f1e43755ad
soaares <raphael.soaresit@gmail.com>
2017-07-13 14:55:19 -0300

    Desafio Realizado

---
+++ desafio_55.py
@@ -0,0 +1,6 @@
+# Desafio 55
+
+# Tuplas são como listas, só que usamos ( ) ao invés de [ ] como usamos
+# em listas. Outra diferença é que os valores das tuplas não podem ser
+# alteradas individualmente, como em listas.
+
