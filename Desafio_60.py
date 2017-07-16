f261a8cb3de2c60d7b963ee67736592f535a9193
soaares <raphael.soaresit@gmail.com>
2017-07-16 13:40:17 -0300

    Desafio Corrigido

---
+++ Desafio_60.py
@@ -0,0 +1,5 @@
+# Desafio 60
+
+# Identação é o espaço para identificar um bloco de códigos, seja ele
+# um loop, teste condicionais com if e etc. A identação é caracterizada
+# por 4 espaço até o inicio do bloco de códigos.
