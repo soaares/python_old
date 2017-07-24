83212297b487406fd853466f09894b18240b3d21
soaares <raphael.soaresit@gmail.com>
2017-07-24 13:29:59 -0300

    Desafio Corrigido

---
+++ desafio_83.py
@@ -0,0 +1,14 @@
+# Desafio 83
+
+idade = 65
+valor1 = 0
+valor2 = 5
+valor3 = 10
+if idade < 4:
+    print("Valor do ingresso: R$" + str(valor1))
+elif idade < 18:
+    print("Valor do ingresso: R$" + str(valor2))
+elif idade < 65:
+    print("Valor do ingresso: R$" + str(valor3))
+elif idade >= 65:
+    print("Valor do ingresso: R$" + str(valor2))
