1a89823c6b5fb623f6e0b3374b1bc070065f012f
soaares <raphael.soaresit@gmail.com>
2017-07-24 10:02:52 -0300

    Desafios Realizados

---
+++ desafio_84.py
@@ -0,0 +1,17 @@
+
+
+ingredientes_pizza = ["queijo","calabresa","tomate","catupiry"]
+
+if "queijo" in ingredientes_pizza:
+    print("Queijo adicionado.")
+if "calabresa" in ingredientes_pizza:
+    print("Calabresa adicionada.")
+if "tomate" in ingredientes_pizza:
+    print("Tomate adicionado.")
+if "catupiry" in ingredientes_pizza:
+    print("Catupiry adicionado.")
+
+# No exemplo acima, temos a lista de ingredientes da pizza, e uma 
+# uma sequência de IF's, o primeiro: caso, o elemento "queijo"
+# conste na lista "ingredientes_pizza", imprima a msg "queijo adicionado!"
+# e funciona da mesma forma para os outros IFs.
---
+++ desafio_85.py
@@ -0,0 +1,15 @@
+# Desafio 85
+
+ingredientes_pizza = ["queijo","calabresa","tomate","catupiry"]
+
+if "queijo" in ingredientes_pizza:
+    print("Queijo adicionado.")
+elif "calabresa" in ingredientes_pizza:
+    print("Calabresa adicionada.")
+elif "tomate" in ingredientes_pizza:
+    print("Tomate adicionado.")
+elif "catupiry" in ingredientes_pizza:
+    print("Catupiry adicionado.")
+
+# No exemplo acima, quando a primeira condição é suprida, o interpretador
+# não executa mais nenhum teste além do primeiro teste passar.
